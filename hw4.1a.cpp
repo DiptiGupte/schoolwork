029f70ff0473d176fa310b03b0d0adf629da90a8
Dipti Gupte <gupte.d50@gmail.com>
2017-11-12 17:00:29 -0800

    double loop

---
+++ hw4.1a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i = 0; i < 5; i++)
+    {
+        cout<<"start ";
+        for(int j = 0; j < 8; j++)
+        {
+            if(j <= i)
+            {
+                cout<<"o";
+            }
+            else
+            {
+                cout<<"x";
+            }
+        }
+        cout<<" end"<<endl;
+    }
+    return 0;
+}
